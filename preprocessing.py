6299ea61082587d5f48e9175e26e904f43abd817
ZuckXu <zaikunxu1989@gmail.com>
2016-04-06 18:55:41 +0200

    update process

--- preprocessing.py
+++ preprocessing.py
@@ -1,38 +1,53 @@
 import numpy as np
 import os
 from PIL import Image
+from collections import defaultdict
+
 #from skimage.filters import sobel 
 train_path = './cars_train/'
 train_files = os.listdir(train_path)
 train_files = [ f for f in train_files if f.endswith('jpg')]
 
-def preprocessing(image, basewidth):
-	
+def getBox():
+	lines = open('celldata.dat', 'r').readlines()
+	img2box = defaultdict(list)  
+	for line in lines : 
+		tmp = line.strip().split()
+		box = tmp[:4]
+		box = [ float(x) for x in box]
+		img2box[tmp[5]] = box
+	return img2box 
+
+
+def image_resize(image, basewidth, img2box):
 	img = Image.open(image).convert('L')
-	img = np.asarray(img.getdata(),dtype=np.float32).reshape((img.size[0],img.size[1]))
-	#img = np.asarray(img)
-	#try :
-        w, h = img.shape
-	#except :
-	#	img = np.asarray([img, img, img]).transpose(1,2,0)
-	#	w, h , x= img.shape
-	if w > h : 
-		npad = ( (0,0), (w- h, 0))
-		img = np.pad(img, npad, 'constant' )
-	else:
-		npad = ((h-w, 0), (0,0))
-		img = np.pad(img, npad, 'constant')
-	newImage = Image.fromarray(img)
-	r = newImage.resize((basewidth, basewidth), Image.ANTIALIAS)
-	r = np.array(r)/255.0
- 	r = sobel(r.astype(np.float32))
+	box[:2] = box[:2] - 16
+	box[2:] = box[2:] + 16
+	img = Image.crop(img, img2box[image])
+	w, h = img.size
+
+	#img = np.asarray(img.getdata(),dtype=np.float32).reshape((img.size[0],img.size[1])
+
+	# if w > h : 
+	# 	npad = ( (0,0), (w- h, 0))
+	# 	img = np.pad(img, npad, 'constant' )
+	# else:
+	# 	npad = ((h-w, 0), (0,0))
+	# 	img = np.pad(img, npad, 'constant')
+	# newImage = Image.fromarray(img)
+	#r = newImage.resize((basewidth, basewidth), Image.ANTIALIAS)
+	#r = np.array(r)/255.0
+	r = img.resize((basewidth, basewidth))
+	r = np.array(r)
 	return r 
 
-train = np.zeros([len(train_files), 224,224])
-for i,f in enumerate(train_files): 
-        if i % 100==0 : print i
-	train[i] = image_resize(train_path + train_files[i], 224)
+def preproces():
+	img2box = getBox()
+	train = np.zeros([len(train_files), 224,224])
+	for i,f in enumerate(train_files): 
+    	if i % 100==0 : print i
+		train[i] = image_resize(train_path + train_files[i], 224, img2box)
+	print train.shape
+	np.save('car_train.npy', train)
 
-print train.shape
-np.save('car_train.npy', train)
 #image_resize(train_path + '00001.jpg', 224)
