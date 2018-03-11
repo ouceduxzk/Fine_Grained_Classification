050d1b57345c3dea8788628c7bc7edaa583a110b
ZuckXu <zaikunxu1989@gmail.com>
2018-03-11 14:53:04 +0100

    bug fix

--- RA-CNN/caffe_racnn/include/caffe/layers/rank_loss2_layer.hpp
+++ RA-CNN/caffe_racnn/include/caffe/layers/rank_loss2_layer.hpp
@@ -24,11 +24,10 @@ public:
     }
 
 protected:
-   // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-   //                        const vector<Blob<Dtype>*>& top);
-   //
-   // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-   //                           const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                           const vector<Blob<Dtype>*>& top);
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
     virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
                              const vector<Blob<Dtype>*>& top);
